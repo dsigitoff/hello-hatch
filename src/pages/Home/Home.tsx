d40977a86aed8b7cd3e7be52d509b4c4b47ca778
dsigitoff <golovsha@gmail.com>
2021-08-28 17:24:01 +0300

    setup prettier and linter

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,15 +5,14 @@ import { Table } from "components/Table";
 
 import "./Home.css";
 
-
 const Home = () => {
   return (
-      <Box className="Home" display="flex">
-        <Input className="HomeInput" placeholder="Enter city name..." />
-        <Container>
-          <Table />
-        </Container>
-      </Box>
+    <Box className="Home" display="flex">
+      <Input className="HomeInput" placeholder="Enter city name..." />
+      <Container>
+        <Table />
+      </Container>
+    </Box>
   );
 };
 
