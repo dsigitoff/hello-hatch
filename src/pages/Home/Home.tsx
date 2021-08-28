4e07542fdabf991ba95ae6b00e1933d919b62545
dsigitoff <golovsha@gmail.com>
2021-08-28 16:14:02 +0300

    setup up environment

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Table} from "components/Table";
+
+const Home = () => {
+    return (
+        <div>
+            <Table/>
+            Home page
+        </div>
+    );
+};
+
+export default Home;
