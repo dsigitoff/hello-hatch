ae6992b4b2b1e6e91822f6c30ecea15fcaf3fa84
dsigitoff <golovsha@gmail.com>
2021-08-28 20:21:15 +0300

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { SyntheticEvent, useCallback, useState } from "react";
 import { Box, Input } from "@material-ui/core";
 import { Column, Table } from "react-virtualized";
 import "react-virtualized/styles.css";
 
-import cities from "assets/nl.json";
+import citiesData from "assets/nl.json";
 
 import "./Home.css";
 
 const Home = () => {
+  const [cities, setCities] = useState(citiesData);
+
+  const handleOnSearch = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setCities(
+        citiesData.filter((el) => el["city"].includes(event?.target.value))
+      );
+    },
+    [cities]
+  );
+
   return (
     <Box className="Home" display="flex">
-      <Input className="HomeInput" placeholder="Enter city..." />
+      <Input
+        className="HomeInput"
+        placeholder="Enter city..."
+        onChange={handleOnSearch}
+      />
       <Table
         width={500}
         height={800}
